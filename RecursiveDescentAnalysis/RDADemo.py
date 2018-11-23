44a5e430cf77b5f788bc7a33583e0162b904d807
Miosolo <xht2012.jn@gmail.com>
2018-11-24 00:15:00 +0800

    modify demo

--- RecursiveDescentAnalysis/RDADemo.py
+++ RecursiveDescentAnalysis/RDADemo.py
@@ -3,5 +3,5 @@ import RDAnalysis_test
 if __name__ == "__main__":
     ts = RDAnalysis_test.Test_RDA()
 
-    print('\nParsing source3.txt:')
-    ts.test_run_source_3()
+    print('\nParsing source2.txt:')
+    ts.test_run_source_2()
