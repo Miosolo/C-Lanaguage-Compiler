f307dadc98a975bbccf5771b1a6c900a7ba764c6
Miosolo <xht2012.jn@gmail.com>
2018-12-08 16:17:21 +0800

    搞定了FIRST集合

---
+++ Predict/FirstCollection_test.py
@@ -0,0 +1,23 @@
+import FirstCollection
+
+def test_first_1():
+  V_T = {'+', '*', '(', ')', 'eps', 'i'}
+  V_N = {'E', 'E\'', 'F', 'T', 'T\''}
+  prodDict = {
+    'E': [['T', 'E']],
+    'E\'': [['+', 'T', 'E\''], ['eps']],
+    'T': [['F', 'T\'']],
+    'T\'': [['*', 'F', 'T\''], ['eps']],
+    'F': [['(', 'E', ')'], ['i']]
+  }
+
+  expectFIRST = {
+    'E': {'(', 'i'},
+    'E\'': {'+', 'eps'},
+    'T': {'(', 'i'},
+    'T\'': {'*', 'eps'},
+    'F': {'(', 'i'}
+  }
+
+  actualFIRST = FirstCollection.getFirstCollection(prodDict, V_N, V_T)
+  assert actualFIRST == expectFIRST
