42dbedb20e70ede3857524b37c5f92c6045317f6
Miosolo <xht2012.jn@gmail.com>
2019-11-07 16:39:19 +0800

    finished lexer

--- simplest-java-lexer/lexer.py
+++ simplest-java-lexer/lexer.py
@@ -1,10 +1,8 @@
-# %%
 # read the file
 CODE_PATH = 'simplest-java-lexer/sample.java'
 with open(CODE_PATH) as f:
   codelines = f.readlines()
 
-# %%
 # rules
 keywords = ['abstract', 'assert', 'boolean', 'break', 'byte',
             'case', 'catch', 'char', 'class', 'const', 'continue',
@@ -15,29 +13,31 @@ keywords = ['abstract', 'assert', 'boolean', 'break', 'byte',
             'public', 'return', 'strictfp', 'short', 'static', 'super',
             'switch', 'synchronized', 'this', 'throw', 'throws',
             'transient', 'try', 'void', 'volatile', 'while']
-delimiters = ['{', '}', '(', ')', '"', '"']
 symbols = ['+', '-', '*', '/', '%', '++', '--',
            '==', '!=', '>', '<', '>=', '<=',
            '&', '|', '^', '~', '<<', '>>', '>>>',
            '&&', '||', '!',
-           '=', '+=', '-=', '*=', '/=', '&=', '<<=', '>>=', '&=', '^=', '|=']
+           '=', '+=', '-=', '*=', '/=', '&=', '<<=', '>>=', '&=', '^=', '|=',
+           '{', '}', '(', ')', '[', ']', '"', '"', ';', '.', ',']
 # todo: 3-tuple, instanceof
-idchars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+idchars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-           'y', 'z', '_'}
+           'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+           'W', 'X', 'Y', 'Z','_'}
 numchars = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-symchars = {'!', '"', '%', '&', "'", '(', ')', '*', '+', '-', '/', '<', '=',
-            '>', '^', '{', '|', '}', '~'}
-blankchars = {' ', '\t'}
+symchars = {'!', '"', '%', '&', "'", '(', ')', '*', '+', '-', '/', '<', '=', ',',
+            '>', '^', '{', '|', '}', '~', '{', '}', '(', ')', '"', '"', ';', '.', '[', ']',}
+blankchars = {' ', '\t', '\n'}
 
-# %%
+
 def stateful_parse(state, c):
   ''' (state, char) -> (tuple(or None), next_state)'''
   if state == 'start':
     if c in idchars:
       return None, 'id'
     elif c in numchars:
-      return None, 'num'
+      return None, 'int'
     elif c in symchars - {'"'}:
       return None, 'sym'
     elif c == '"':
@@ -47,25 +47,80 @@ def stateful_parse(state, c):
     else:
       raise RuntimeError
   elif state == 'id':
-    if c in idchars + numchars:
+    if c in idchars | numchars:
       return None, 'id'
     elif c in blankchars:
-      return (position, parsed, 'keyword' if parsed in keywords else 'identifier'), 'start'
+      return (parsed, 'keyword' if parsed in keywords else 'identifier'), 'start'
     elif c in symchars:
-      return (position, parsed, 'keyword' if parsed in keywords else 'identifier'), 'sym'
+      return (parsed, 'keyword' if parsed in keywords else 'identifier'), 'sym'
     else:
       raise RuntimeError
-    
+  elif state == 'int':
+    if c in numchars:
+      return None, 'int'
+    elif c == '.':
+      return None, 'float'
+    elif c in symchars:
+      return (parsed, 'number'), 'sym'
+    elif c in blankchars:
+      return (parsed, 'number'), 'start'
+    else:
+      raise RuntimeError
+  elif state == 'float':
+    if c in numchars:
+      return None, 'float'
+    elif c in symchars:
+      return (parsed, 'number'), 'sym'
+    elif c in blankchars:
+      return (parsed, 'number'), 'start'
+    else:
+      raise RuntimeError
+  elif state == 'sym':
+    #comment
+    if parsed+c == '//':
+      return None, 'comment'
+    # greedy
+    elif parsed+c in symbols:
+      return None, 'sym'
+    elif c == '"':
+      return (parsed, 'symbol'), 'str'
+    elif c in symbols:
+      return (parsed, 'symbol'), 'sym'
+    elif c in blankchars:
+      return (parsed, 'symbol'), 'start'
+    elif c in idchars:
+      return (parsed, 'symbol'), 'id'
+    elif c in numchars:
+      return (parsed, 'symbol'), 'int'
+    else:
+      raise RuntimeError
+  elif state == 'str':
+    if c != '"':
+      return None, 'str'
+    else:
+      return (parsed+'"', 'string'), 'start'
+  else:
+    raise RuntimeError
 
-# %%
+
 # tuple: (position, class, value)
 tokens = []
 for l in range(len(codelines)):
-  line = codelines[l].strip()
-  curword = ''
-  breakflag = False
+  line = codelines[l]
+  parsed = ''
+  state = 'start'
 
   for i in range(len(line)):
-    # firstly, try to parse an identifier
-    c = l[i]
+    token, state = stateful_parse(state, line[i])
+    if token is not None:
+      tokens.append(((l+1, i), *token))
+      parsed = ''
+    if state == 'comment':
+      parsed = ''
+      state = 'start'
+      break
+    if state != 'start':
+      parsed += line[i]
 
+for t in tokens:
+  print(t)
